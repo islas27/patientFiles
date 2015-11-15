f8ce8afb9970923cf3177180aad37e7fe1344be1
Jesus Jose Garcia Pardo <a271305@uach.mx>
2015-11-15 12:54:12 -0700

    Se agrega el modelo Etiqueta

---
+++ app/models/Etiqueta.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package models;
+
+import javax.persistence.Entity;
+import play.db.jpa.Model;
+
+/**
+ *
+ * @author jesus
+ */
+@Entity
+public class Etiqueta extends Model{
+
+    public static enum Tags{
+        Patologico, Vicio, Inmunizacion
+
+    }
+
+    public String name;
+    public String label;
+    public String tipo;
+
+    public Etiqueta (String name, String label, Tags tipo){
+        this.name = name;
+        this.label = label;
+        switch (tipo) {
+            case Patologico:
+                this.tipo = "Patologico";
+                break;
+            case Vicio:
+                this.tipo = "Vicio";
+                break;
+            case Inmunizacion:
+                this.tipo = "Inmunizacion";
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+
+}
