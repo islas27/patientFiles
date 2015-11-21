34afe6b859b7bc15c64c8120e313b0dfcad26eca
Jesus Jose Garcia Pardo <a271305@uach.mx>
2015-11-21 12:37:36 -0700

    Se crean controladores CRUD para todos los modelos

---
+++ app/controllers/crud/ExpedienteMedicos.java
@@ -0,0 +1,17 @@
+package controllers.crud;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import controllers.CRUD;
+
+/**
+ *
+ * @author jesus
+ */
+public class ExpedienteMedicos extends CRUD{
+
+}
