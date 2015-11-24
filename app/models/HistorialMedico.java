0ea8003683777c90f1275144d8dcb5882dba8d34
Jesus Jose Garcia Pardo <a271305@uach.mx>
2015-11-23 17:06:17 -0700

    Se alteran modelos Usuario e HistorialMedico

--- app/models/HistorialMedico.java
+++ app/models/HistorialMedico.java
@@ -6,6 +6,8 @@
 
 package models;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Reference;
 import java.util.Date;
@@ -23,40 +25,48 @@ public class HistorialMedico extends Model{
     public String antecedentesFamilires;
     public String higientePersonal;
     public Boolean Embarazo;
-    public Integer mes;
-    public String rutaClinica;
-    public Blob diagrama;
-    public String descripcion;
-    public Date fecha;
-    public Blob evidencia;
+    public Integer trimestre;
+//    public String rutaClinica;
+//    public Blob diagrama;
+//    public String descripcion;
+    public String fecha;
+//    public Blob evidencia;
 
     @Reference
     public ExpedienteMedico expediente;
 
 
-    @Reference
-    public Proceso primerProceso;
+//    @Reference
+//    public Proceso primerProceso;
 
     public String doctor;
 
-    public HistorialMedico(String motivoConsulta, String antecedentesFamilires, String higientePersonal, Boolean Embarazo, Integer mes, String rutaClinica, Blob diagrama, String descripcion, Date fecha, Blob evidencia, ExpedienteMedico expediente, Proceso primerProceso, String doctor) {
+    public HistorialMedico(String motivoConsulta, String antecedentesFamilires, String higientePersonal, Boolean Embarazo, Integer trimestre, String descripcion, LocalDate fecha, ExpedienteMedico expediente, String doctor) {
         this.motivoConsulta = motivoConsulta;
         this.antecedentesFamilires = antecedentesFamilires;
         this.higientePersonal = higientePersonal;
         this.Embarazo = Embarazo;
-        this.mes = mes;
-        this.rutaClinica = rutaClinica;
-        this.diagrama = diagrama;
-        this.descripcion = descripcion;
-        this.fecha = fecha;
-        this.evidencia = evidencia;
+        this.trimestre = trimestre;
+        this.setFechaDate(fecha);
         this.expediente = expediente;
-        this.primerProceso = primerProceso;
         this.doctor = doctor;
     }
 
     public String toString(){
         return this.motivoConsulta;
+    }
+
+    public LocalDate getFechaDate(){
+        try{
+            return LocalDate.parse(this.fecha, DateTimeFormatter.ISO_DATE);
+        }catch (Exception ex) {
+            return null;
+        }
+    }
+
+    public void  setFechaDate(LocalDate date){
+        DateTimeFormatter formater = DateTimeFormatter.ISO_DATE;
+        this.fecha = date.format(formater);
     }
 
 }
